49f4cc1a8eab047238baccd3a155d90d25e5124e
Dobrocsi Kornél <dobrocsi.kornel@szaleziakk.local>
2023-09-11 09:27:18 +0200

    main

--- src/gondolat_olvaso/Gondolat_olvaso.java
+++ src/gondolat_olvaso/Gondolat_olvaso.java
@@ -1,20 +1,65 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package gondolat_olvaso;
 
-/**
- *
- * @author dobrocsi.kornel
- */
+import java.util.Random;
+import java.util.Scanner;
+
+
 public class Gondolat_olvaso {
 
-    /**
-     * @param args the command line arguments
-     */
+    static Scanner scr = new Scanner(System.in);
+    static Random rnd=new Random();
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+         // for(int i=0; i<3; i++){
+        String[] pakli=Kirak();
+        int megoldas = Melyik();
+        Kever(pakli);
+        //   }
+        EzVolt(megoldas);
+    }
+
+    private static String[] Kirak() {
+        String[] pakli = new String[24];
+        String[] szinek = {"P", "T", "Z", "M"};
+        String[] ertekek = {"Asz", "Kir", "Fel", "X", "IX", "VIII"};
+        int s = 0;
+        int e = 0;
+        int lap = 0;
+        while (s < szinek.length) {
+            while (e < ertekek.length) {
+                pakli[lap] = szinek[s] + ertekek[e];
+                //System.out.println(pakli[lap]);
+                e++;
+                lap++;
+            }
+            e = 0;
+            s++;
+        }
+        int x=0;
+        for (int y=0;y<8;y++){
+            for (int i=0;i<3;i++){
+                System.out.printf("%-8s",pakli[x]);
+             x++;
+            }
+            System.out.println();
+        }
+        return pakli;
+    }
+
+    private static int Melyik() {
+        System.out.print("adj meg egy oszlopot 1-3: ");
+        int valasztott = scr.nextInt();
+        System.out.println("");
+        return valasztott;
+    }
+
+    private static void EzVolt(int megoldas) {
+        System.out.println(megoldas + " Ez a szam volt a megoldas");
+    }
+
+    private static void Kever(String[] pakli) {
+        
     }
     
 }
