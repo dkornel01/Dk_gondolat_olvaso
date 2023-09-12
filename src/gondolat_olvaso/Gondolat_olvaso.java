a3edd1e6e4a21175b3e45756269b3b3ca1aa2733
dkornel01 <115635103+dkornel01@users.noreply.github.com>
2023-09-12 21:31:02 +0200

    test

--- src/gondolat_olvaso/Gondolat_olvaso.java
+++ src/gondolat_olvaso/Gondolat_olvaso.java
@@ -5,46 +5,39 @@ import java.util.Scanner;
 
 
 public class Gondolat_olvaso {
-  static   String[] pakli = new String[22];
+        
     static Scanner scr = new Scanner(System.in);
+    static String[] pakli = new String[22];
+    static String[] lehet = new String[7];
     
     public static void main(String[] args) {
-         // for(int i=0; i<3; i++){
+        int oszlop=0;
+
         Kirak();
-        int megoldas = Melyik();
-        Kever(megoldas);
-        //   }
-        int x=0;
-         for (int y=0; y<7; y++){
-            for (int i=0;i<3;i++){
-                System.out.printf("%-8s",pakli[x]);
-             x++;
-            }
-            System.out.println();
+        for (int kor=0;kor<3;kor++){
+        oszlop = Melyik();
+        String[] tarto=new String[22];
+        tarto=toltes(tarto,pakli);
+        lapvalaszto(oszlop,kor);
+        Kever(oszlop,pakli,tarto);
+        kiiras(pakli);
         }
-        EzVolt(megoldas);
+        EzVolt();
     }
 
-    private static String[] Kirak() {
-      
+    private static void Kirak() {
+        
         String[] szinek = {"P", "T", "Z", "M"};
         String[] ertekek = {"Asz", "Kir", "Fel", "X", "IX", "VIII"};
-        int i=0;
+        int i=1;
         for (String szin:szinek){
             for (int o=0;i<22 && o<ertekek.length;o++){
                 pakli[i++]=szin+"_"+ertekek[o];
             }
         }
-        int x=0;
-        for (int y=0; y<7; y++){
-            for (int k=0;k<3;k++){
-                System.out.printf("%-8s",pakli[x]);
-             x++;
-            }
-            System.out.println();
+        kiiras(pakli);
         }
-        return pakli;
-    }
+    
 
     private static int Melyik() {
         int oszlop=1;
@@ -57,35 +50,158 @@ public class Gondolat_olvaso {
         return oszlop;
     }
 
-    private static void EzVolt(int megoldas) {
-        System.out.println(megoldas + " Ez a szam volt a megoldas");
+    private static void EzVolt() {
+        System.out.println(lehet[0] + " Ez a szam volt a megoldas");
     }
 
-    private static void Kever(int oszlop) {
-        String[] paklik=pakli;
+    private static void Kever(int oszlop,String[] pakli,String[] tarto) {
         switch(oszlop){
             case(1): 
-                for (int i = 1; i < 7; i++) {
-                pakli[i] = paklik[20-(i-1)*3];
-                pakli[i+7] = paklik[19-(i-1)*3];
-                pakli[i+14] = paklik[21-(i-1)*3];
-                    
+                kiiras(tarto);
+                for (int i = 1; i < 8; i++) {
+                pakli[i] = tarto[20-(i-1)*3];
+                pakli[i+7] = tarto[19-(i-1)*3];
+                pakli[i+14] = tarto[21-(i-1)*3];
+                //kiiras(pakli);
+                //System.out.println("hiba");
                 }
+                kiiras(tarto);
+                break;
         case(2): 
-        for (int i = 1; i < 7; i++) {
-                pakli[i] = paklik[19-(i-1)*3];
-                pakli[i+7] = paklik[20-(i-1)*3];
-                pakli[i+14] = paklik[21-(i-1)*3];
+                for (int i = 1; i < 8; i++) {
+                pakli[i] = tarto[19-(i-1)*3];
+                pakli[i+7] = tarto[20-(i-1)*3];
+                pakli[i+14] = tarto[21-(i-1)*3];
                 }
+                break;
         case (3):
-            for (int i = 1; i < 7; i++) {
-                pakli[i] = paklik[21-(i-1)*3];
-                pakli[i+7] = paklik[20-(i-1)*3];
-                pakli[i+14] = paklik[19-(i-1)*3];
+                for (int i = 1; i < 8; i++) {
+                pakli[i] = tarto[21-(i-1)*3];
+                pakli[i+7] = tarto[20-(i-1)*3];
+                pakli[i+14] = tarto[19-(i-1)*3];
                 }
+                break;
             }
             
+        }
+        private static void kiiras(String[] paklik){
+            int x=1;
+            for (int y=0; y<7; y++){
+                for (int i=0;i<3;i++){
+                    System.out.printf("%-8s",paklik[x]);
+                 x++;
+                }
+                System.out.println(x);
+            }
+            System.out.println("-----------");
         }    
+
+    private static String[] toltes(String[] tarto, String[] pakli) {
+        int x=0;
+        while (x!=pakli.length){
+            tarto[x]=pakli[x];
+            x++;
+        }
+        return tarto;
     }
+
+    private static void lapvalaszto(int oszlop,int kor) {
+        switch(oszlop){
+            case(1):
+                if (kor==0){
+                    int i=1;
+                    int x=0;
+                    while (i<22){
+                        if (i%3==1){
+                            lehet[x]=pakli[i];
+                            x++;
+                        }
+                        i++;
+                    }
+                }
+                else{
+                    String[] atmeneti=new String[7];
+                    int i=1;
+                    int x=0;
+                    while (i<22){
+                        if (i%3==1){
+                            int kereses=0;
+                            while (kereses<lehet.length){
+                                if (lehet[kereses]==pakli[i]){
+                                    atmeneti[x]=pakli[i];
+                                }
+                                kereses++;
+                            }
+                            x++;
+                        }
+                        i++;
+                    }
+                }
+                break;
+            case(2):
+                if (kor==0){
+                    int i=1;
+                    int x=0;
+                    while (i<22){
+                        if (i%3==2){
+                            lehet[x]=pakli[i];
+                            x++;
+                        }
+                        i++;
+                    }
+                }
+                else{
+                    String[] atmeneti=new String[7];
+                    int i=1;
+                    int x=0;
+                    while (i<22){
+                        if (i%3==2){
+                            int kereses=0;
+                            while (kereses<lehet.length){
+                                if (lehet[kereses]==pakli[i]){
+                                    atmeneti[x]=pakli[i];
+                                }
+                                kereses++;
+                            }
+                            x++;
+                        }
+                        i++;
+                    }
+                }
+                break;
+            case(3):
+                if (kor==0){
+                    int i=1;
+                    int x=0;
+                    while (i<22){
+                        if (i%3==0){
+                            lehet[x]=pakli[i];
+                            x++;
+                        }
+                        i++;
+                    }
+                }
+                else{
+                    String[] atmeneti=new String[7];
+                    int i=1;
+                    int x=0;
+                    while (i<22){
+                        if (i%3==0){
+                            int kereses=0;
+                            while (kereses<lehet.length){
+                                if (lehet[kereses]==pakli[i]){
+                                    atmeneti[x]=pakli[i];
+                                }
+                                kereses++;
+                            }
+                            x++;
+                        }
+                        i++;
+                    }
+                }
+                break;
+        }
+    }
+}
     
 
